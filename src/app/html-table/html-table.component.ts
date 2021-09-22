54d8a543305a98a788b1af5e5bf2efc960dad9db
Michael Shields <michael.shields@keysight.com>
2021-09-22 10:10:30 -0600

    add async pipe with loading

--- src/app/html-table/html-table.component.ts
+++ src/app/html-table/html-table.component.ts
@@ -9,26 +9,11 @@ import { RootObject } from 'src/app/interfaces/searchResults';
   styleUrls: ['./html-table.component.scss']
 })
 export class HtmlTableComponent implements OnInit {
-    totalAngularPackages: number  = 0 ;
-
-    packages: any[];
     baseObject$: Observable<RootObject>
 
     constructor(private readonly httpService: HttpService) { }
 
     public ngOnInit(): void {
-        // this.getProductsUsingSubscribeMethod();
-         this.getProductsUsingAsyncPipe();
+        this.baseObject$ =this.httpService.getSearchResults();
     }
-
-    public getProductsUsingSubscribeMethod() {
-        this.httpService.getSearchResults().subscribe((data: RootObject) => {
-            this.packages =  data.results;
-            this.totalAngularPackages = data.total;
-        });
-    }
-
-    public getProductsUsingAsyncPipe() {
-            this.baseObject$ =this.httpService.getSearchResults();
-    }
-  }
+}
