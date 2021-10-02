c7be71f8b1786e6bfa6cb00a541a2eb171641801
Michael Shields <michael.shields@keysight.com>
2021-10-02 12:32:55 -0600

    add comment

--- src/app/subscriptions/subscribing-observables/manual-subscription.component.ts
+++ src/app/subscriptions/subscribing-observables/manual-subscription.component.ts
@@ -17,16 +17,20 @@ export class ManualSubscriptionsComponent implements OnInit {
     thirdSubscription: Subscription;
   
     ngOnInit() {
+        // we don't need to worry about the first two values since they return only one value
       getSingleValueObservable()
         .subscribe(value => this.first = value);
   
       getDelayedValueObservable()
         .subscribe(value => this.second = value);
-  
+
+        // the third one continues to send values
+        // need to create a subscription so it can be unsubscribed when the component is descroyed
+        // to avoid memory leaks
       this.thirdSubscription = getMultiValueObservable()
         .subscribe(value => this.third = value);
     }
-  
+
     // Multi value observables must manually
     // unsubscribe to prevent memory leaks.
     ngOnDestroy() {
