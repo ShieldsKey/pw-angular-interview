40b60379e9592866547a64356a77bd14d1edf004
Michael Shields <michael.shields@keysight.com>
2021-10-05 15:37:29 -0600

    add waiting indicator

--- src/app/html-table/html-table.component.ts
+++ src/app/html-table/html-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
 import { HttpService } from '../http.service';
 import { Result, RootObject } from '../interfaces/searchResults';
 
@@ -8,12 +9,15 @@ import { Result, RootObject } from '../interfaces/searchResults';
   styleUrls: ['./html-table.component.scss']
 })
 export class HtmlTableComponent implements OnInit {
+    public loading = true;
     public results: Result[];
     public totalAngularPackages: number;
     constructor(private readonly httpService: HttpService) { }
 
     public ngOnInit(): void {
-        this.httpService.getSearchResults().subscribe((data: RootObject) => {
+        this.httpService.getSearchResults().pipe(
+            finalize(() => this.loading = false)
+            ).subscribe((data: RootObject) => {
             this.results =  data.results;
             this.totalAngularPackages = data.total;
         });
