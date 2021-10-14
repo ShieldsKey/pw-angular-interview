5e617104828c19bc236f06d4cf0a1eb4ff2386a4
Michael Shields <michael.shields@keysight.com>
2021-10-14 08:26:48 -0600

    remove httpclientmodule for start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,8 +19,7 @@ import { RouterModule } from '@angular/router';
     MatTableModule,
     BrowserModule,
     AppRoutingModule,
-    RouterModule,
-    HttpClientModule
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
