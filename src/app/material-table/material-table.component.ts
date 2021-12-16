1e26b118cb3cecc902dad277254b8a5370df9f05
Michael Shields <michael.shields@keysight.com>
2021-12-16 07:29:28 -0700

    change detectiong

--- src/app/material-table/material-table.component.ts
+++ src/app/material-table/material-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
 import { finalize } from 'rxjs/operators';
 import { Result, RootObject } from 'src/app/interfaces/searchResults';
 import { HttpService } from '../../app/http.service';
@@ -8,20 +9,38 @@ import { HttpService } from '../../app/http.service';
   templateUrl: './material-table.component.html',
   styleUrls: ['./material-table.component.scss']
 })
-export class MaterialTableComponent implements OnInit {
-    loading = false
+export class MaterialTableComponent implements OnInit, AfterViewInit {
+    @ViewChild('dataTable', { static: false }) public dataTable!: MatTable<any>;
+    @ViewChild('dataTable', { static: true }) public dataTableStatic!: MatTable<any>;
+    loadingData = true
     dataSource: any[];
     displayedColumns: string[] = ['name', 'scope', 'version', 'date'];
-    constructor(private readonly httpService: HttpService) { }
+    constructor(private readonly httpService: HttpService, private readonly changeDetection: ChangeDetectorRef) { }
 
     public ngOnInit(): void {
-        this.loading = true;
+        this.loadingData = true;
         this.httpService.getSearchResults().pipe(
-            finalize(() => this.loading = false)
+            finalize(() => {
+                this.loadingData = false;
+                console.log('-----------------data table')
+                console.log('static: false', this.dataTable);
+                console.log(this.dataTableStatic);
+                this.changeDetection.detectChanges();
+
+                console.log('-----------------after change detection')
+                console.log('static: false', this.dataTable);
+                console.log(this.dataTableStatic);
+             })
         ).subscribe((data: RootObject) => {
             this.dataSource = data.results.map((result: Result) => {
                 return result.package;
             });
         });
+    }
+
+    public ngAfterViewInit(): void {
+        console.log('-----------------ngAfterViewInit')
+        console.log('static: false', this.dataTable);
+        console.log(this.dataTableStatic);
     }
 }
